150a67865d4d9f8c7a0b2c2a40b0709d53fd7f3b
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-08-17 12:57:46 +0200

    Implemented messages read functionality

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'Use strict';
+
+//Update number of unread notifications
+const unreadNotNum = document.getElementById('unread--num');
+const updateNotNum = function () {
+  unreadNotNum.textContent = document.querySelectorAll('.unread').length;
+};
+updateNotNum();
+
+//Mark all notifications as read
+
+const allRead = document.getElementById('read');
+allRead.addEventListener('click', () => {
+  const unread = Array.from(document.querySelectorAll('.unread'));
+  unread.forEach((unr) => unr.classList.remove('unread'));
+
+  updateNotNum();
+});
+
+//Mark notification read on click
+const container = document.querySelector('.container');
+container.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (!event.target.closest('.notification')) return;
+  const notification = event.target.closest('.notification');
+
+  if (!notification.classList.contains('unread')) return;
+  const unreadDot = notification.querySelector('.unread--dot');
+  notification.classList.remove('unread');
+  unreadDot.classList.add('hide--dot');
+
+  updateNotNum();
+});
